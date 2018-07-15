8822ceda0f77ed48f9a586fe18edb68a4c2d6995
Josh Long <starbuxman@gmail.com>
2018-07-15 02:40:03 -0700

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ APP_NAME=ttd-content
 JOB_NAME=${APP_NAME}
 
 cf d -f ${APP_NAME}
-cf push -b static_buildpack -p . ${APP_NAME}
+cf push -b staticfile_buildpack -p . ${APP_NAME}
 cf set-health-check $APP_NAME none
 
 cf restage ${APP_NAME}
