0c65cd43de374f1a63a4d2e6fcc9216c4c2b84e1
Josh Long <starbuxman@gmail.com>
2018-07-15 02:29:41 -0700

    travis

---
+++ deploy/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+APP_NAME=ttd-content
+JOB_NAME=${APP_NAME}
+
+cf d -f ${APP_NAME}
+cf push -b static_buildpack -p . ${APP_NAME}
+cf set-health-check $APP_NAME none
+
+cf restage ${APP_NAME}
